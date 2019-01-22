1a1b00ab6a251143c96fe52bf669624e80776f94
Nasa Quba <nasaquba@gmail.com>
2019-01-22 10:05:46 -0800

    db model

--- database/db.js
+++ database/db.js
@@ -1,15 +1,61 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('woolly','root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  operatorsAliases: false,
+const Member = require('./model.js')
+const bcrypt = require('bcryptjs')
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+
+module.exports = {
+  Create: function(uname, passwd) {
+    return new Promise((resolve, reject) => {
+      Member.create({username:uname, password: passwd})
+      .then((membr) => {
+        membr.set('password',passwd)
+        resolve('create')
+      })
+    })
+
+  },
+
+  Read: function(uname, plainPwd) {
+    Member.find(
+      {where: {username: uname}}).then((m) => {
+      let dbhash = m.getDataValue('password')
+      // console.log(m.getDataValue('password', 'aabc'))
+      console.log(bcrypt.compareSync(plainPwd, dbhash));
+    })
+  },
+  
+  Update: function() {
+
+
+  },
+
+  Delete: function() {
+
   }
-});
+ }
 
-module.exports = sequelize;
+
+
+
+Member.create({username:'Naassa', password:"weewewe23i3u3"})
+.then((membr) => {
+  // membr.setDataValue('username')
+  membr.set('password', '112')
+})
+
+
+
+Member.find(
+  {where: {id: 1}}).then((m) => {
+  let dbhash = m.getDataValue('password')
+  console.log(m.getDataValue('password', 'aabc'))
+  console.log(bcrypt.compareSync("wdddewewe23i3u3", dbhash));
+
+})
+
+
+// Member.find(
+//   {where: {id: 1}}).then((m) => {
+//   // console.log(m.set('username', '9999'))
+//   console.log(m.get('username'))
+//   // console.log(m.getDataValue('username'))
+// })
--- database/model.js
+++ database/model.js
@@ -1,31 +1,43 @@
-const sequelize = require('./db.js');
 const Sequelize = require('sequelize');
+const bcrypt = require('bcryptjs')
+var pwhash;
+
+
+// Create database woolly
+const sequelize = new Sequelize('woolly','root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
 
 
 
-console.log('uuuuuuuu')
-// table members
+// Create  members table
 const Member = sequelize.define('member', {
   username: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
-  password: {type: Sequelize.STRING, allowNull: false}
+  password: {
+    type: Sequelize.STRING, 
+    allowNull: false,
+    unique: true,
+    set: function(val) {
+      let salt = bcrypt.genSaltSync(10);
+      pwhash = bcrypt.hashSync(val, salt);
+      this.setDataValue('password', pwhash);
+    }
+  } 
 
 })
 
+Member.sync()
+module.exports = Member
 
-
-Member.sync({force: true}).then(() => {
-  // Table created
-  return Member.create({
-    username: 'John',
-    password: 'Hancock'
-  });
-});
-
-Member.create({username:'Naassa', password:"weewewe23i3u3"})
-.then(membr => {
-  console.log('uuuuuuuu')
-  console.log(membr.get('username'))
-})

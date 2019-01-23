afab51244868e753b5d03a603cabe2987190cc49
Nasa Quba <nasaquba@gmail.com>
2019-01-22 22:47:14 -0800

    aded fe prt

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,79 +4,61 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 
-function InputText(props) {
-  return(
-    <div> 
-      <TextField placeholder={props.type} type={props.type}/>
-    </div>
-  )
-    
-}
-
 function LoginGrid (props) {
   return (
     <div>
       <Grid container spacing={24}>
-  
+        
+        <Grid item xs={6} sm={6}>
+        </Grid>
+
+        <Grid item xs={6} sm={3}>
+          <TextField placeholder='username' type='text'  onChange={props.handleText} />
+        </Grid>
+
         <Grid item xs={6} sm={3}>
         </Grid>
 
         <Grid item xs={6} sm={3}>
-          <InputText type='text' />
-        </Grid>
-
-  
-        <Grid item xs={6} sm={3}>
-        </Grid>
-
-        <Grid item xs={6} sm={3}>
-          <InputText type='password' />
+          <TextField placeholder='password' type='password'  onChange={props.handleText} />
         </Grid>
 
       </Grid>
     </div>
   )
 }
-
-
-{/* 
-      <Grid container spacing={12}>
-        <Grid item xs>
-        </Grid>
-        <Grid item xs={8} >
-          <InputText type='text' txt='ussernamee'/> 
-        </Grid>
-        <Grid item xs>
-        </Grid>s
-      </Grid>
-      
-      <Grid container spacing={12}>
-        <Grid item xs>
-        </Grid>
-        <Grid item xs={6}>
-          <TextField type="password" txt='paasswrrd'/>
-        </Grid>
-        <Grid item xs>
-        </Grid>
-      </Grid> 
-    </div>
-  )
-  */}
-
-
 class Login extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      uname: '',
+      pwd: '',
+    }
+  }
 
+
+  handleText(e) {
+    debugger
+    this.setState({
+      uname: e.target.value})
   }
+
   render() {
+    debugger
+    if (this.state.uname !== '') {
+      debugger
+    }
     return (
-    <div>
-      <LoginGrid />
-    </div>
-)
+      <div>
+        <LoginGrid handleText={this.handleText} />
+      </div>
+    )
   }
+
+
 }
+
+
 
 
 export default Login;
